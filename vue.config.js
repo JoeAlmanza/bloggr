6f7e534009589de4b8ca8751f1183aaf8f1a288b
Joe Almanza <joealmanza2113@gmail.com>
2020-09-20 19:42:39 -0600

    build

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  outputDir: "../client",
-  productionSourceMap: false
-};
-
-module.exports = {
   outputDir: "docs",
   publicPath: process.env.NODE_ENV === 'production'
     ? '/DarkSoulsClickerGame/'
